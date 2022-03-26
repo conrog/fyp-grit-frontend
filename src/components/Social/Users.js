90c25a32d42822492dda8e9bcdca8ef0f41aa68e
conrog <c17730535@mydit.ie>
2022-03-26 18:37:39 +0000

    [GRIT-19] User Search

--- src/components/Social/Users.js
+++ src/components/Social/Users.js
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from "react";
 import api from "../../api/api";
 import UserList from "./UserList";
+import LoadingSpinner from "../Common/LoadingSpinner";
 
 function Users() {
   const [users, setUsers] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
     const { token } = JSON.parse(sessionStorage.getItem("token"));
+    setLoading(true);
     api
       .get("/users", {
         headers: {
@@ -14,11 +19,28 @@ function Users() {
       })
       .then((res) => {
         setUsers([...res.data]);
+        setLoading(false);
       })
       .catch((error) => {
         console.log(error);
       });
   }, []);
+
+  const userSearch = async (searchValue) => {
+    setLoading(true);
+    try {
+      const { token } = JSON.parse(sessionStorage.getItem("token"));
+      let { data } = await api.get(`/users/${searchValue.toLowerCase()}`, {
+        headers: {
+          Authorization: "Basic " + token,
+        },
+      });
+      setUsers(data);
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+  };
 
   return (
     <div>
@@ -31,16 +53,30 @@ function Users() {
               type="text"
               placeholder="Username..."
               className="light-border p-1 md:w-10/12"
+              onChange={(event) => {
+                setSearchValue(event.target.value);
+              }}
             />
-
-            <input type="submit" className="btn md:w-2/12" value="Search" />
+            <input
+              type="submit"
+              className="btn md:w-2/12"
+              value="Search"
+              onClick={(event) => {
+                event.preventDefault();
+                userSearch(searchValue);
+              }}
+            />
           </div>
         </form>
       </div>
       {/* Users List */}
       <div>
         <h2 className="text-lg font-semibold">Users</h2>
-        <UserList users={users} />
+        {loading ? (
+          <LoadingSpinner />
+        ) : (
+          <UserList users={users} searchValue={searchValue} />
+        )}
       </div>
     </div>
   );
