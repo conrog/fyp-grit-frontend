04c43c4d8bc49e1dfcdc2c97f666eba7223cd548
conrog <c17730535@mydit.ie>
2022-04-05 18:51:45 +0100

    fixed files for deployment

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-// const API_URL = process.env.REACT_APP_GRIT_API;
-
-//For mobile testing
-const API_URL = process.env.REACT_APP_GRIT_API_LOCAL_NETWORK;
+const API_URL = process.env.REACT_APP_GRIT_API;
 
 const api = axios.create({
   baseURL: `${API_URL}`,
